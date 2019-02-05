eae4db5b246bbe7c23b8fbf8b7cc3337da1a0e12
caioavelar <caioavelar17@hotmail.com>
2019-02-05 17:56:08 -0200

    Restricao de conteudo: cliente so recupera seus pedidos

--- src/main/java/com/caio/cursomc/repositories/PedidoRepository.java
+++ src/main/java/com/caio/cursomc/repositories/PedidoRepository.java
@@ -1,11 +1,19 @@
 package com.caio.cursomc.repositories;
 
+import org.springframework.transaction.annotation.Transactional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.caio.cursomc.domain.Cliente;
 import com.caio.cursomc.domain.Pedido;
 
 @Repository //Anotação de repositório						//Tipo de objeto, tipo do identificador
 public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
 	//Interface, classe que não pode ser instanciada diretamente
+	
+	@Transactional(readOnly=true)
+	Page<Pedido> findByCliente(Cliente cliente, Pageable pageRequest);
 }
--- src/main/java/com/caio/cursomc/services/PedidoService.java
+++ src/main/java/com/caio/cursomc/services/PedidoService.java
@@ -4,9 +4,13 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.caio.cursomc.domain.Cliente;
 import com.caio.cursomc.domain.ItemPedido;
 import com.caio.cursomc.domain.PagamentoComBoleto;
 import com.caio.cursomc.domain.Pedido;
@@ -14,6 +18,8 @@ import com.caio.cursomc.domain.enums.EstadoPagamento;
 import com.caio.cursomc.repositories.ItemPedidoRepository;
 import com.caio.cursomc.repositories.PagamentoRepository;
 import com.caio.cursomc.repositories.PedidoRepository;
+import com.caio.cursomc.security.UserSS;
+import com.caio.cursomc.services.exception.AuthorizationException;
 import com.caio.cursomc.services.exception.ObjectNotFoundException;
 
 @Service
@@ -33,11 +39,13 @@ public class PedidoService {
 	
 	@Autowired
 	private ClienteService clienteService;
+	
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
 	
 	@Autowired
 	private EmailService emailService;
+	
 	public Pedido find(Integer id) { //Método que utiliza
 										  //um objeto do repositório para pesquisar
 		Optional<Pedido> obj = repo.findById(id);
@@ -62,9 +70,19 @@ public class PedidoService {
 			ip.setPreco(ip.getProduto().getPreco());
 			ip.setPedido(obj);
 		}
+		
 		itemPedidoRepository.saveAll(obj.getItens());
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
 	}
 	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		UserSS user = UserService.authenticated();
+		if(user == null) {
+			throw new AuthorizationException("Acesso negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
+	}
 }
